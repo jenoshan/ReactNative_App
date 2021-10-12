252d73aef383be8a9f79e0524126c67df334f94d
Jenoshan <jenoshan4744@gmail.com>
2021-10-12 09:36:24 +0530

    dashboard page corrections

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -8,9 +8,22 @@ import { RootTabScreenProps } from '../types';
 export default function Dashboard({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.separator}>
+        <View style={styles.colorbox1}></View>
+        <Text style={styles.title}>Maths - G/08</Text>
+      </View>
+      <View style={styles.separator}>
+        <View style={styles.colorbox2}></View>
+        <Text style={styles.title}>Science - G/08</Text>
+      </View>
+      <View style={styles.separator}>
+        <View style={styles.colorbox3}></View>
+        <Text style={styles.title}>Tamil - G/08</Text>
+      </View>
+      <View style={styles.separator}>
+        <View style={styles.colorbox4}></View>
+        <Text style={styles.title}>English - G/08</Text>
+      </View>
     </View>
   );
 }
@@ -19,15 +32,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 5
   },
   separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    paddingVertical: 10,
+    borderWidth : 2,
+    borderRadius : 10,
+    borderColor: "#05445E",
+    padding: 20,
+    margin : 10,
+    width: 300,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  colorbox1: {
+    backgroundColor: '#189AB4',
+    borderRadius : 10,
+    height: 75,
+    width: 300,
+    marginTop: -10
+  },
+  colorbox2: {
+    backgroundColor: '#81B622',
+    borderRadius : 10,
+    height: 75,
+    width: 300,
+    marginTop: -10
+  },
+  colorbox3: {
+    backgroundColor: '#B68D40',
+    borderRadius : 10,
+    height: 75,
+    width: 300,
+    marginTop: -10
+  },
+  colorbox4: {
+    backgroundColor: '#EF7C8E',
+    borderRadius : 10,
+    height: 75,
+    width: 300,
+    marginTop: -10
   },
 });
